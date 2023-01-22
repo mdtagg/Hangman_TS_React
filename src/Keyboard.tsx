01fd997a4e80fa599fd11aee088e84aefff3d785
Mike Tagg <mdtagg91@gmail.com>
2023-01-21 16:40:45 -0800

    keyboard styling and letters added

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -2,9 +2,14 @@
 import React from "react"
 
 const Keyboard = () => {
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
     return (
         <div className='keyboard-container'>
-            test
+            {letters.map((letter,index) => {
+                return ( 
+                    <button key={index} className='letter-btn'>{letter}</button>
+                )
+            })}
         </div>
     )
 }
