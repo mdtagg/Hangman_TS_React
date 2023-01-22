6eb4ae8ba58a99d2a57336df1dff8685142568f3
Mike Tagg <mdtagg91@gmail.com>
2023-01-21 17:06:30 -0800

    divs used to create gallows in HangmanGallows component

--- src/HangmanGallows.tsx
+++ src/HangmanGallows.tsx
@@ -3,7 +3,44 @@ import React from 'react'
 const HangmanGallows = () => {
     return (
         <div className='gallows-container'>
-            test
+            <div style={{
+                height:'20px',
+                width:'150px',
+                backgroundColor:'black',
+                top:'20px',
+                left:'170px',
+                position:'relative'
+            }}
+            >
+            </div>
+            <div style={{
+                height:'30px',
+                width:'20px',
+                backgroundColor:'black',
+                position:'relative',
+                top:'40px',
+                left:'150px'
+            }}>
+
+            </div>
+            <div style={{
+                height:'300px',
+                width:'20px',
+                backgroundColor:'black',
+                top:'40px',
+                position:'relative'
+            }}>
+            </div>
+            <div style={{
+                height:'20px',
+                width:'200px',
+                backgroundColor:'black',
+                position:'relative',
+                top:'340px',
+                right:'110px'
+            }}>
+
+            </div>
         </div>
     )
 }
