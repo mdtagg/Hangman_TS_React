f7c899da6497e1c1a6913f169b4d1d3bfc882248
Mike Tagg <mdtagg91@gmail.com>
2023-01-21 17:28:18 -0800

    hangmans head body and legs added

--- src/HangmanGallows.tsx
+++ src/HangmanGallows.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
 
+const HEAD = <div style={{
+            width:'50px',
+            height:'50px',
+            backgroundColor:'black',
+            borderRadius:'100%',
+            position:'relative',
+            left:'335px',
+            top:'65px'
+        }}></div>
+
+const BODY = <div style={{
+            width:'25px',
+            height:'100px',
+            backgroundColor:'black',
+            position:'relative',
+            left:'347px',
+            top:'60px'
+}}></div>
+
+const LEFT_LEG = <div style={{
+    width:'25px',
+    height:'70px',
+    backgroundColor:'black',
+    position:'relative',
+    left:'320px',
+    top:'50px',
+    rotate:'45deg'
+}}></div>
+
+const RIGHT_LEG = <div style={{
+    width:'25px',
+    height:'70px',
+    backgroundColor:'black',
+    position:'relative',
+    left:'370px',
+    bottom:'25px',
+    rotate:'135deg'
+}}></div>
+
+const BODY_PARTS = [HEAD,BODY,LEFT_LEG,RIGHT_LEG]
+
 const HangmanGallows = () => {
     return (
         <div className='gallows-container'>
+            <div>
+            {BODY_PARTS.slice(0)}
+            </div>
             <div style={{
                 height:'20px',
                 width:'150px',
@@ -21,7 +65,6 @@ const HangmanGallows = () => {
                 top:'40px',
                 left:'150px'
             }}>
-
             </div>
             <div style={{
                 height:'300px',
