c72988327203715ae21f0025ca3e945c66e2c0e6
Mike Tagg <mdtagg91@gmail.com>
2023-01-23 13:42:30 -0800

    guessedLetters passed in, visibilty style changed
    visibiilty is changed through a ternary operation that checks to see
    if the lette that has been pressed is included in the word to be guessed

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -3,14 +3,21 @@ import React from "react"
 
 type HangmanWordProps = {
     wordToGuess:string
+    guessedLetters:string[]
 }
 
-const HangmanWord = ({wordToGuess}:HangmanWordProps) => {
+const HangmanWord = ({wordToGuess,guessedLetters}:HangmanWordProps) => {
+    
     return (
         <div className='word-container'>
             {wordToGuess.split('').map(letter => (
                 <span className='underline'>
-                    <span className='letter'>
+                    <span 
+                    className='letter'
+                    style={{ 
+                        visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden'
+                     }}
+                    >
                         {letter}
                     </span>
                 </span>
