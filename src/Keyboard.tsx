997034893c09a95d02f89ba1bdcd60de7587efc3
Mike Tagg <mdtagg91@gmail.com>
2023-01-23 13:43:39 -0800

    onClick function added
    the onClick function runs the addGuessedLetter function in app.tsx

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,13 +1,23 @@
 
-import React from "react"
+type KeyboardProps = {
+    addGuessedLetter: (letter:string) => void
+}
 
-const Keyboard = () => {
+const Keyboard = ({addGuessedLetter}:KeyboardProps) => {
     const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
+
     return (
         <div className='keyboard-container'>
             {letters.map((letter,index) => {
                 return ( 
-                    <button key={index} className='letter-btn'>{letter}</button>
+                    <button 
+                    key={index} 
+                    value={letter} 
+                    className='letter-btn' 
+                    onClick={() => addGuessedLetter(letter)}
+                    >
+                        {letter}
+                    </button>
                 )
             })}
         </div>
