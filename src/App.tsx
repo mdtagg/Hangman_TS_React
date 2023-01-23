d41027ef8dc2440f57b2c83639f94702def25e93
Mike Tagg <mdtagg91@gmail.com>
2023-01-23 12:01:45 -0800

    wordToBeGuessed state added, function getWord added
    wordToBeGuessed is the state of the word obtained in getWord
    getWord gets a word from the wordList.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import HangmanGallows from './HangmanGallows'
 import HangmanWord from './HangmanWord'
 import Keyboard from './Keyboard'
+import wordList from './wordList.json'
 import './App.css'
 
 const App = () => {
+    function getWord() {
+        return wordList[Math.floor(Math.random() * wordList.length)].toUpperCase()
+    }
+
+    const [ wordToGuess, setWordToGuess ] = useState(getWord)
+
     return (
         <div className='hangman-container'>
             <HangmanGallows/>
-            <HangmanWord/>
+            <HangmanWord wordToGuess={wordToGuess}/>
             <Keyboard/>
         </div>
     )
