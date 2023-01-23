80bc37027dccf68e1e813ceb174d5ca75d5ecc8e
Mike Tagg <mdtagg91@gmail.com>
2023-01-23 15:39:43 -0800

    keyboard modules css file added, styling added for different button states
    a css module file was added to dynamically change the class names
    of the keyboard buttons.

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,20 +1,33 @@
+import styles from './Keyboard.module.css'
+
 
 type KeyboardProps = {
     addGuessedLetter: (letter:string) => void
+    incorrectLetters: string[]
+    correctLetters:string[]
 }
 
-const Keyboard = ({addGuessedLetter}:KeyboardProps) => {
+const Keyboard = ({addGuessedLetter,incorrectLetters,correctLetters}:KeyboardProps) => {
     const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
-
+    // console.log({wordToGuess,incorrectLetters})
     return (
         <div className='keyboard-container'>
             {letters.map((letter,index) => {
+                const correct = correctLetters.includes(letter)
+                const incorrect = incorrectLetters.includes(letter)
                 return ( 
                     <button 
-                    key={index} 
-                    value={letter} 
-                    className='letter-btn' 
-                    onClick={() => addGuessedLetter(letter)}
+                        key={index} 
+                        value={letter} 
+                        disabled={incorrect || correct}
+                        className={
+                        `${styles.btn}
+                        ${incorrect ? styles.incorrect : ''}
+                        ${correct ? styles.correct : ''}
+                        `
+                        }
+                        onClick={() => addGuessedLetter(letter)
+                    }
                     >
                         {letter}
                     </button>
