c62b4ab7994f3b760e3f771c4f5cb18c52a765b8
Mike Tagg <mdtagg91@gmail.com>
2023-01-23 14:28:22 -0800

    win or loss message added
    a div was added above the components that checks for win or loss
    conditions and renders a message if either of the conditios is met

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,30 @@ const App = () => {
 
     const [ wordToGuess, setWordToGuess ] = useState(getWord)
     const [ guessedLetters,setGuessedLetters ] = useState<string[]>([])
+    // console.log(wordToGuess)
 
     const addGuessedLetter = (letter:string) => {
         if(guessedLetters.includes(letter)) return
         setGuessedLetters([...guessedLetters,letter])
-        console.log(guessedLetters)
     }
+
+    const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+    const winGame = wordToGuess.split('').every(letter => guessedLetters.includes(letter))
+    console.log({wordToGuess,guessedLetters})
 
     return (
         <div className='hangman-container'>
-            <HangmanGallows/>
+            <div 
+                className='end-message'
+                style={{visibility: 
+                    incorrectLetters.length === 6 || winGame ? 
+                    'visible' :
+                    'hidden'
+                }}
+            >
+                {incorrectLetters.length === 6 ? 'You Lose, Try Again?': 'You Win!, Another Round?' }
+            </div>
+            <HangmanGallows numberOfGuesses={incorrectLetters.length}/>
             <HangmanWord 
                 wordToGuess={wordToGuess}
                 guessedLetters={guessedLetters}
